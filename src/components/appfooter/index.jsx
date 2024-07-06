0670ba14f23ee4597d7a248425e963725c74cff9
Reuben Muli <mulimumo36@gmail.com>
2024-07-07 02:01:38 +0300

    modify the footer

--- src/components/appfooter/index.jsx
+++ src/components/appfooter/index.jsx
@@ -1,5 +1,22 @@
+import { Typography } from 'antd';
+
 const Appfooter = () => {
-  return <div className="appfooter">welcome</div>;
+  return (
+    <div className='appfooter'>
+      <Typography.Link href='https://www.google.com' target={'_blank'}>
+        Privacy policy
+      </Typography.Link>
+      <Typography.Link href='https://www.google.com' target={'_blank'}>
+        Terms & Conditions
+      </Typography.Link>
+      <Typography.Link href='https://www.google.com' target={'_blank'}>
+        Return policy
+      </Typography.Link>
+      <Typography.Link href='tel:+254 708377276' target={'_blank'}>
+        +254 708377276
+      </Typography.Link>
+    </div>
+  );
 };
 
 export default Appfooter;
